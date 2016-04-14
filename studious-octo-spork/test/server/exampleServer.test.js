59fcd254eb5ccfd6347dfd6c3299db186290d2f0
tlplu <tlp@loveyou.lu>
2016-04-14 17:13:10 +0300

    The initial testing file structure

---
+++ studious-octo-spork/test/server/exampleServer.test.js
@@ -0,0 +1,8 @@
+// Note: Arrow function use with Mocha is discouraged.
+// (see http://mochajs.org/#arrow-functions)
+describe('my initial server test', function () {
+  it('does something that should be tested', function () {
+    // This code will be executed by the test driver when the app is started
+    // in the correct mode
+  })
+})
