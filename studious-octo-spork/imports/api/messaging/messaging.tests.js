7b3d6ead6c392729763b10afe741d942621c5b35
Sotiris Lamprinidis <sot.lampr@gmail.com>
2016-04-18 19:06:39 +0300

    Deleted some leaked files

--- studious-octo-spork/imports/api/messaging/messaging.tests.js
+++ studious-octo-spork/imports/api/messaging/messaging.tests.js
@@ -1,6 +1,5 @@
 /* eslint-env mocha */
 import { Meteor } from 'meteor/meteor';
-import { Random } from 'meteor/random';
 import { assert } from 'meteor/practicalmeteor:chai'
 
 import { Messages } from './messaging.js'
@@ -9,30 +8,6 @@ import { saveMessage } from './methods.js';
 if (Meteor.isServer) {
   describe('Messaging', () => {
     describe('methods', () => {
-      const userIdTo = Random.id();
-      const userIdFrom = Random.id();
-      let messageId;
-
-      beforeEach(() => {
-        Messages.remove({});
-        // Provide a data object for creating new entries
-        data = {
-          to: userIdTo,
-          from: userIdFrom,
-          message: 'test message',
-          dateCreated: new Date(),
-          read: false,
-          visible: true
-        }
-        // Create a new entry by default
-        messageId = Messages.insert(data);
-      });
-
-      it('can insert a new message', () => {
-        saveMessage._execute({ userIdFrom }, { data });
-        assert.isNotNone(Messages.findOne({to: userIdFrom}),
-            "messaging.saveMessage did not insert a new message");
-      });
     });
   });
-}
+};
