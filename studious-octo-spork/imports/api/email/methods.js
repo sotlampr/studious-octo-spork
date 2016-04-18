4b315e72595e9eaca01f72768b07ec1d70b33e23
tlplu <tlp@loveyou.lu>
2016-04-18 19:28:43 +0300

    Validation for sendTestEmail method

--- studious-octo-spork/imports/api/email/methods.js
+++ studious-octo-spork/imports/api/email/methods.js
@@ -1,13 +1,23 @@
 import { Meteor } from 'meteor/meteor';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import { Email } from 'meteor/email';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { ValidationError } from 'meteor/mdg:validation-error';
 
 
 export const sendTestEmail = new ValidatedMethod({
   // DOES NOT WORK WITH WRONG EMAIL INFO
   name: 'email.sendTestEmail',
-  // TODO: validation!
-  validate: null,
+
+  validate: new SimpleSchema({
+    name: { type: String },
+    email: {
+        type: String,
+        regEx: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    },
+    comment: { type: String }
+  }).validator(),
+
   run (data) {
     var bodytext = "Someone '" + data.name + "' with email: " + data.email + " sent you the comment: " + data.comment + " !";
     Email.send({
