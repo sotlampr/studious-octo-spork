ed532f18e6bd6b7ac77b3a5db4dfead567a6f187
tlplu <tlp@loveyou.lu>
2016-05-03 21:12:37 +0300

    Client tests for searching

--- studious-octo-spork/imports/ui/client/users.tests.js
+++ studious-octo-spork/imports/ui/client/users.tests.js
@@ -32,14 +32,14 @@ describe('Users page', () => {
       users: Meteor.users.find()
     };
     withRenderedTemplate('usersIndex', data, el => {
-      assert.equal($(el).find('li').length, 3);
+      assert.equal($(el).find('#users li').length, 3);
     });
     done();
   });
 
   it('Displays name and occupation for all 3 default users', function(done) {
     withRenderedTemplate('usersIndex', {}, el => {
-      let users = $(el).find('li');
+      let users = $(el).find('#users li');
       for (let i=0; i < 3; i++) {
         assert.equal(
             users[i].innerText,
@@ -48,5 +48,23 @@ describe('Users page', () => {
       }
     });
     done();
+  });
+
+  it('Displays results after searching for occupation', () => {
+    Session.set('work', usersData[0].profile.occupation);
+    withRenderedTemplate('usersIndex', {}, el => {
+      let user = $(el).find('#occupations li');
+      assert.equal(user[0].innerText, usersData[0].username + ': ' + usersData[0].profile.occupation);
+    });
+    Session.set('work', '');
+  });
+
+  it('Displays results after searching for description', () => {
+    Session.set('description', usersData[0].profile.description);
+    withRenderedTemplate('usersIndex', {}, el => {
+      let user = $(el).find('#descriptions li');
+      assert.equal(user[0].innerText, usersData[0].username + ': ' + usersData[0].profile.occupation);
+    });
+    Session.set('description', '');
   });
 });
