8b619970d47d22eff8fe09a84abf176e4e915c37
Sotiris Lamprinidis <sot.lampr@gmail.com>
2016-05-03 12:51:36 +0300

    A couple of client-side test

---
+++ studious-octo-spork/imports/ui/client/users.tests.js
@@ -0,0 +1,70 @@
+import { assert } from 'meteor/practicalmeteor:chai';
+import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
+import { _ } from 'meteor/underscore';
+import { Mongo } from 'meteor/mongo';
+
+import '../users.js';
+import { withRenderedTemplate } from '../test-helpers.js';
+import { StubCollections } from '../test-helpers.js';
+
+describe('Users page', () => {
+  let usersData;
+
+  beforeEach((done) => {
+    StubCollections.stub(Meteor.users);
+    usersData = ([
+      {
+        username: 'bill',
+        profile: {
+          occupation: 'mechanic'
+        }
+      },
+      {
+        username: 'john',
+        profile: {
+          occupation: 'artist'
+        }
+      },
+      {
+        username: 'mary',
+        profile: {
+          occupation: 'martial arts'
+        }
+      }
+    ]);
+    _.each(usersData, (data) => {
+      Meteor.users.insert(data);
+    });
+    // Template.registerHelper('_', key => key);
+    done();
+  });
+
+  afterEach((done) => {
+    StubCollections.restore();
+    Template.deregisterHelper('_');
+    done();
+  });
+
+  it('Displays 3 default users', (done) => {
+    const data = {
+      users: Meteor.users.find()
+    };
+    withRenderedTemplate('usersIndex', data, el => {
+      assert.equal($(el).find('li').length, 3);
+    });
+    done();
+  });
+
+  it('Displays name and occupation for all 3 default users', () => {
+    const data = {
+      users: Meteor.users.find()
+    };
+    withRenderedTemplate('usersIndex', data, el => {
+      let users = $(el).find('li');
+      assert.equal(users[0].innerText, 'bill: mechanic');
+      assert.equal(users[1].innerText, 'john: artist');
+      assert.equal(users[2].innerText, 'mary: martial arts');
+    });
+  });
+});
--- studious-octo-spork/imports/ui/test-helpers.js
+++ studious-octo-spork/imports/ui/test-helpers.js
@@ -1,7 +1,10 @@
+import { Mongo } from 'meteor/mongo';
 import { Template } from 'meteor/templating';
 import { _ } from 'meteor/underscore';
 import { Blaze } from 'meteor/blaze';
 import { Tracker } from 'meteor/tracker';
+
+const SYMBOLS = _.keys(Mongo.Collection.prototype);
 
 const withDiv = function withDiv(callback) {
   const el = document.createElement('div');
@@ -22,3 +25,42 @@ export const withRenderedTemplate = function withRenderedTemplate(template, data
   });
 };
 
+const stubPair = function(pair) {
+  SYMBOLS.forEach((symbol) => {
+    sinon.stub(pair.collection, symbol, _.bind(pair.localCollection[symbol], pair.localCollection));
+  });
+};
+
+const restorePair = function(pair) {
+  SYMBOLS.forEach((symbol) => {
+    pair.collection[symbol].restore();
+  });
+};
+
+export const StubCollections = {
+  _pairs: {},
+  _collections: [],
+  add(collections) {
+    StubCollections._collections.push(...collections);
+  },
+  stub(collections) {
+    collections = collections || StubCollections._collections;
+    [].concat(collections).forEach((collection) => {
+      if (!StubCollections._pairs[collection._name]) {
+        const options = {transform: collection._transform};
+
+        const pair = {
+          localCollection: new collection.constructor(null, options),
+          collection
+        };
+
+        stubPair(pair);
+        StubCollections._pairs[collection._name] = pair;
+      }
+    });
+  },
+  restore: function() {
+    _.each(StubCollections._pairs, restorePair);
+    StubCollections._pairs = {};
+  }
+};
