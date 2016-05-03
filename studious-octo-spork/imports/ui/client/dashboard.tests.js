14df4f8e7bbaaa216a4c10e24c836897a7fa17c2
Sotiris Lamprinidis <sot.lampr@gmail.com>
2016-05-03 18:58:12 +0300

    More tests for dashboard, sinon package for stubbing

--- studious-octo-spork/imports/ui/client/dashboard.tests.js
+++ studious-octo-spork/imports/ui/client/dashboard.tests.js
@@ -1,21 +1,55 @@
+import { Meteor } from 'meteor/meteor';
 import { assert } from 'meteor/practicalmeteor:chai';
 import { Template } from 'meteor/templating';
 import { Tracker } from 'meteor/tracker';
 import { Random } from 'meteor/random';
 import { $ } from 'meteor/jquery';
+import { sinon } from 'meteor/practicalmeteor:sinon';
 
 import '../dashboard.js';
 import { withRenderedTemplate } from '../test-helpers.js';
 import { StubCollections } from '../test-helpers.js';
 
 describe('Dashboard', function() {
+  let usersData;
+  let usersIdArray;
+
   beforeEach((done) => {
+    StubCollections.stub(Meteor.users);
+    usersIdArray = []
+    usersData = ([
+      {
+        username: 'bill',
+        profile: {
+          occupation: 'mechanic',
+          description: 'Je suis un mechanic'
+        }
+      },
+      {
+        username: 'john',
+        profile: {
+          occupation: 'artist',
+          description: 'John Ross'
+        }
+      },
+      {
+        username: 'mary',
+        profile: {
+          occupation: 'martial arts',
+          description: 'Exarcheia resident'
+        }
+      }
+    ]);
+    _.each(usersData, (data) => {
+      usersIdArray.push(Meteor.users.insert(data));
+    });
     Template.registerHelper('_', key => key);
     done();
   });
 
-  afterEach(function(done) {
-    Template.deregisterHelper('formData');
+  afterEach((done) => {
+    StubCollections.restore();
+    Template.deregisterHelper('_');
     done();
   });
 
@@ -25,5 +59,15 @@ describe('Dashboard', function() {
       assert.include(targetText, "You are not registered yet.");
       done();
     });
+  });
+
+  it('Display appropriate data when logged in', function (done) {
+    sinon.stub(Meteor, 'userId', () => { return usersIdArray[0] });
+    withRenderedTemplate('dashboard', {}, el => {
+      assert.equal($(el).find('#username').val(), usersData[0].username);
+      assert.equal($(el).find('#occupation').val(), usersData[0].profile.occupation);
+      assert.equal($(el).find('#description').val(), usersData[0].profile.description);
+    });
+    done();
   });
 });
