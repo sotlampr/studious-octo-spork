54ebbfc9b9376193300c1b51afff1ae16ded47cf
tlplu <tlp@loveyou.lu>
2016-05-04 19:22:26 +0300

    Client tests for navigation bar

--- studious-octo-spork/imports/ui/client/body.tests.js
+++ studious-octo-spork/imports/ui/client/body.tests.js
@@ -2,6 +2,7 @@ import { assert } from 'meteor/practicalmeteor:chai';
 import { Template } from 'meteor/templating';
 import { $ } from 'meteor/jquery';
 
+import { FlowRouter } from 'meteor/kadira:flow-router';
 import '../body.js';
 import { withRenderedTemplate } from '../test-helpers.js';
 
@@ -21,6 +22,56 @@ describe('Home page', () => {
       let targetText = $(el).find('p#credits').text();
       assert.include(targetText, "touch_code");
       done();
+    });
+  });
+
+  describe('Navigation bar', () => {
+    it('click on Home', (done) => {
+      withRenderedTemplate('App_body', {}, el => {
+        $(el).find('#homePg')[0].click();
+        assert.equal(FlowRouter.current().path, '/');
+        done();
+      });
+    });
+
+    it('click on About', (done) => {
+      withRenderedTemplate('App_body', {}, el => {
+        $(el).find('#aboutPg')[0].click();
+        assert.equal(FlowRouter.current().path, '/about');
+        done();
+      });
+    });
+
+    it('click on Users', (done) => {
+      withRenderedTemplate('App_body', {}, el => {
+        $(el).find('#usersPg')[0].click();
+        assert.equal(FlowRouter.current().path, '/users');
+        done();
+      });
+    });
+
+    it('click on Dashdoard', (done) => {
+      withRenderedTemplate('App_body', {}, el => {
+        $(el).find('#dashboardPg')[0].click();
+        assert.equal(FlowRouter.current().path, '/dashboard');
+        done();
+      });
+    });
+
+    it('click on Contact', (done) => {
+      withRenderedTemplate('App_body', {}, el => {
+        $(el).find('#contactPg')[0].click();
+        assert.equal(FlowRouter.current().path, '/contact');
+        done();
+      });
+    });
+
+    it('click on Sign Up / Sign In ', (done) => {
+      withRenderedTemplate('App_body', {}, el => {
+        $(el).find('#signUpInPg')[0].click();
+        assert.equal(FlowRouter.current().path, '/register');
+        done();
+      });
     });
   });
 });
