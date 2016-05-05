442f9660e75756e3c0df85c4c43867354d4f1a39
Sotiris Lamprinidis <sot.lampr@gmail.com>
2016-05-05 15:06:35 +0300

    Style and consistency fixes

--- studious-octo-spork/imports/ui/client/body.tests.js
+++ studious-octo-spork/imports/ui/client/body.tests.js
@@ -6,72 +6,72 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import '../body.js';
 import { withRenderedTemplate } from '../test-helpers.js';
 
-describe('Home page', () => {
-  beforeEach((done) => {
+describe('Home page', function() {
+  beforeEach(function(done) {
     Template.registerHelper('_', key => key);
     done();
   });
 
-  afterEach((done) => {
+  afterEach(function(done) {
     Template.deregisterHelper('_');
     done();
   });
 
-  it('Company name appears on footer', (done) => {
+  it('Company name appears on footer', function(done) {
     withRenderedTemplate('App_body', {}, el => {
       let targetText = $(el).find('p#credits').text();
       assert.include(targetText, "touch_code");
-      done();
     });
+    done();
   });
 
-  describe('Navigation bar', () => {
-    it('click on Home', (done) => {
+  describe('Navigation bar', function() {
+    it('Clicking on Home redirects to "/"', function(done) {
       withRenderedTemplate('App_body', {}, el => {
         $(el).find('#homePg')[0].click();
         assert.equal(FlowRouter.current().path, '/');
-        done();
       });
+      done();
     });
 
-    it('click on About', (done) => {
+    it('Clicking on About redirects to "/about"', function(done) {
       withRenderedTemplate('App_body', {}, el => {
         $(el).find('#aboutPg')[0].click();
         assert.equal(FlowRouter.current().path, '/about');
-        done();
       });
+      done();
     });
 
-    it('click on Users', (done) => {
+    it('Clicking on Users redirects to "/users"', function(done) {
       withRenderedTemplate('App_body', {}, el => {
         $(el).find('#usersPg')[0].click();
         assert.equal(FlowRouter.current().path, '/users');
-        done();
       });
+      done();
     });
 
-    it('click on Dashdoard', (done) => {
+    it('Clicking on Dashdoard redirects to "/dashboard"', function(done) {
       withRenderedTemplate('App_body', {}, el => {
         $(el).find('#dashboardPg')[0].click();
         assert.equal(FlowRouter.current().path, '/dashboard');
-        done();
       });
+      done();
     });
 
-    it('click on Contact', (done) => {
+    it('Clicking on Contact redirects to "/contact"', function(done) {
       withRenderedTemplate('App_body', {}, el => {
         $(el).find('#contactPg')[0].click();
         assert.equal(FlowRouter.current().path, '/contact');
-        done();
       });
+      done();
     });
 
-    it('click on Sign Up / Sign In ', (done) => {
+    it('Clicking on Sign Up / Sign In redirects to "/register"', function(done) {
       withRenderedTemplate('App_body', {}, el => {
         $(el).find('#signUpInPg')[0].click();
         assert.equal(FlowRouter.current().path, '/register');
-        done();
       });
+      done();
     });
   });
 });
