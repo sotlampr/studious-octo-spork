8e98efe52ca7add0f8f41db37c869963754b6204
Sotiris Lamprinidis <sot.lampr@gmail.com>
2016-05-18 22:54:34 +0300

    Account balance on user profile

--- studious-octo-spork/imports/startup/server/accounts-config.js
+++ studious-octo-spork/imports/startup/server/accounts-config.js
@@ -4,5 +4,6 @@ Accounts.onCreateUser(function (options, user) {
     user.profile = user.profile || {};
     user.profile.occupation = '';
     user.profile.description = '';
+    user.profile.balance = 0.0;
     return user;
-  });
+});
