b612238b4e67d537123a7c6075e93257065fb6df
tlplu <tlp@loveyou.lu>
2016-06-13 20:24:21 +0300

    Publish only necessary events

--- studious-octo-spork/imports/api/events/server/publications.js
+++ studious-octo-spork/imports/api/events/server/publications.js
@@ -1,7 +1,21 @@
 import { Meteor } from 'meteor/meteor';
-
 import { Events  } from '../events.js';
 
 Meteor.publish('events', function eventsPublications() {
-  return Events.find();
+  return Events.find({
+    $or: [
+      {
+        $or: [
+          {giverId: this.userId},
+          {receiverId: this.userId}
+        ]
+      },
+      {
+        $and: [
+          {giverValidated: true},
+          {receiverValidated: true}
+        ]
+      }
+    ]
+  });
 })
--- studious-octo-spork/imports/api/suggestions/server/publications.js
+++ studious-octo-spork/imports/api/suggestions/server/publications.js
@@ -1,6 +1,5 @@
-import { Meteor } from 'meteor/meteor';
-
 import { Suggestions  } from '../suggestions.js';
+import { Meteor } from 'meteor/meteor';
 
 Meteor.publish('suggestions', function suggestionsPublications() {
   return Suggestions.find();
