464e61c462c2f0d2ba543f39983582689f178e3b
tlplu <tlp@loveyou.lu>
2016-07-05 18:05:43 +0300

    Changed suggestions/methods.js

--- studious-octo-spork/imports/api/suggestions/methods.js
+++ studious-octo-spork/imports/api/suggestions/methods.js
@@ -1,14 +1,18 @@
 import { Meteor } from 'meteor/meteor';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
-
 import { Suggestions } from './suggestions.js';
 
+
+// saveSuggestion takes an occupation, and if there isn't
+// already in suggestions collection, then insert it
 export const saveSuggestion = new ValidatedMethod({
   name: 'suggestions.saveSuggestion',
+
   validate: new SimpleSchema({
     suggestion: { type: String },
   }).validator(),
+
   run (data) {
     if (Suggestions.find({suggestion: data.suggestion}).count() === 0) {
       Suggestions.insert({
