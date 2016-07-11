949909d2b32e3716c439f69a7467c21008fed848
tlplu <tlp@loveyou.lu>
2016-07-11 18:44:20 +0300

    The same style

--- studious-octo-spork/imports/api/suggestions/methods.js
+++ studious-octo-spork/imports/api/suggestions/methods.js
@@ -4,8 +4,9 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { Suggestions } from './suggestions.js';
 
 
-// saveSuggestion takes an occupation, and if there isn't
-// already in suggestions collection, then insert it
+/*  saveSuggestion takes an occupation, and if there isn't
+*   already in suggestions collection, then insert it
+*/
 export const saveSuggestion = new ValidatedMethod({
   name: 'suggestions.saveSuggestion',
 
--- studious-octo-spork/imports/startup/server/fixtures.js
+++ studious-octo-spork/imports/startup/server/fixtures.js
@@ -38,9 +38,10 @@ let occupations = [
 
 
 Meteor.startup(function () {
-  // USERNAME: Complete email address (website@gmail.com)
-  // PASSWORD: Email password (gmail password)
-  // HOST: Smtp server address (for gmail is smtp.gmail.com, for hotmail is smtp.live.com, ...)
+  /*  USERNAME: Complete email address (website@gmail.com)
+  *   PASSWORD: Email password (gmail password)
+  *   HOST: Smtp server address (for gmail is smtp.gmail.com, for hotmail is smtp.live.com, ...)
+  */
   process.env.MAIL_URL = 'smtp://USERNAME:PASSWORD@HOST:587';
 
   if (Suggestions.find().count() === 0 ) {
