cdd135ed07941b7c87556c4b61efcc63f2d4e61d
tlplu <tlp@loveyou.lu>
2016-07-16 17:05:55 +0300

    Data for pre-filling the edit-profile form

--- studious-octo-spork/imports/ui/settings.js
+++ studious-octo-spork/imports/ui/settings.js
@@ -1,9 +1,41 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { Suggestions } from '../api/suggestions/suggestions.js';
 import './settings.html';
 
 
+Template.settings.onCreated(function settingsOnCreated() {
+  this.subscribe('suggestions');
+});
+
+
 Template.settings.helpers({
+  settings: function () {
+    return {
+      position: "top",
+      limit: 3,
+      rules: [
+        {
+          collection: Suggestions,
+          field: "suggestion",
+          template: Template.autocomplete
+        }
+      ]
+    };
+  },
+
+  formData: function () {
+    // Data for pre-filling the edit-profile form
+    user = Meteor.users.findOne(Meteor.userId());
+    if (user) {
+      return {
+        username: user.username,
+        occupation: user.profile.occupation,
+        description: user.profile.description,
+        email: user.emails[0].address
+      };
+    }
+  },
 });
 
 
